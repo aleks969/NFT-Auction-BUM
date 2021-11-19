b24dcb4808fc8320bbc4b1ff65e52f43fab08ec1
aleks <aleks.mihaylov69@gmail.com>
2021-11-19 11:55:12 +0200

    feat: add product info and update styles

--- src/components/product/ProductInfoCreator.jsx
+++ src/components/product/ProductInfoCreator.jsx
@@ -12,11 +12,27 @@ import styles from "./ProductInfoCreator.module.scss";
 
 export default function ProductInfoCreator({ name, avatar, verified = false }) {
   return (
-    <div className={styles["product-info-creator"]}>
-      <p className={classNames(styles.title)}>Creator</p>
-      <div className={styles["user-container"]}>
-        <User name={name} size={39} avatar={avatar} verified={verified} />
-      </div>
+    <div className={classNames(styles["product-info-creator"])}>
+      <Card className={classNames(styles.card)}>
+        <Container
+          disableGutters
+          className={classNames(styles["title-container"])}
+          sx={{
+            backgroundColor: "#1D193c",
+            padding: "3% 10%",
+            display: "flex",
+          }}
+        >
+          <Typography className={classNames(styles.title)}>Creator</Typography>
+        </Container>
+        <Container
+          disableGutters
+          className={classNames(styles["user-container"])}
+          sx={{ display: "flex" }}
+        >
+          <User avatar={avatar} verified={verified} name={name}></User>
+        </Container>
+      </Card>
     </div>
   );
 }
